fc79b5cb9f684138a6343f130af51bd6452c95b8
khalled alkarmi <khalled_95@gmail.com>
2023-01-24 21:03:21 +0300

    init commit

---
+++ src/test/java/com/kh/moviespring/MoviesSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.moviespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
